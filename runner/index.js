b3b84fa70db6cde35db84e055b5e3b992130e294
Dan Fairaizl <fairaizl@gmail.com>
2018-12-29 20:31:14 -0500

    Add an example to use conditionals

--- runner/index.js
+++ runner/index.js
@@ -3,7 +3,7 @@ const { resolve } = require('path');
 
 const Compiler = require('../compiler');
 
-const sourceFile = resolve(__dirname, '..', 'examples', 'callback.nv');
+const sourceFile = resolve(__dirname, '..', 'examples', 'fib.nv');
 
 const novaCompier = new Compiler(sourceFile, { debugGraph: true });
 novaCompier.compile();
