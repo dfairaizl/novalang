cb9af1b418b9e5a88c65378a0f1421f3e108ef51
Dan Fairaizl <fairaizl@gmail.com>
2019-10-22 10:17:09 -0400

    More graph query WIP

--- compiler/graph/query.js
+++ compiler/graph/query.js
@@ -1,30 +1,69 @@
+class QueryStage {
+  constructor () {
+    this.startNode = null;
+    this.degree = 'out';
+    this.filters = {};
+
+    this.maxDepth = 1;
+    this.depth = 0;
+  }
+}
+
 class Traversal {
   constructor (graph, options) {
     this.graph = graph;
 
-    this.degree = 'out';
-    this.maxDepth = 15; // hops from start
+    const s = new QueryStage();
+    this.stages = [s];
+    this.stageIndex = 0;
+    // this.currentStage = s;
+    // this.stageHead = s;
   }
 
   start (node) {
-    this.startNode = node;
+    // const stage = new QueryStage();
+    this.currentStage.startNode = node;
+    // this.stages.push(stage);
+    // this.currentStage = stage;
   }
 
   direction (d) {
-    this.degree = d;
+    if (!this.currentStage) {
+      const next = new QueryStage();
+      this.stages.push(next);
+    }
+
+    this.currentStage.degree = d;
   }
 
   filter (criteria) {
-    this.filters = criteria;
+    this.currentStage.filters = criteria;
+    // this.filters.push(this.currentFilters);
+    // this.currentFilters = criteria;
+  }
+
+  commitStage () {
+    if (this.currentStage !== null) {
+      // const next = new QueryStage();
+      //
+      // this.stages.push(next);
+      this.stageIndex++;
+    }
   }
 
   // BFS collecting all paths
   run () {
-    if (!this.startNode) {
-      this.startNode = this.graph.nodes[0];
+    debugger;
+    // reset stages
+    this.stageIndex = 0;
+
+    console.log(require('util').inspect(this.stages, { depth: null }));
+
+    if (!this.currentStage.startNode) {
+      this.currentStage.startNode = this.graph.nodes[0];
     }
 
-    const q = [[this.startNode]];
+    const q = [[this.currentStage.startNode]];
     const matchedPaths = [];
 
     while (q.length > 0) {
@@ -33,6 +72,9 @@ class Traversal {
 
       if (this.isDestination(endNode)) {
         matchedPaths.unshift(path);
+        this.nextStage();
+        this.currentStage.depth++;
+        continue;
       }
 
       const neighborsList = this.graph.adjacencyList[endNode.id].edges;
@@ -45,22 +87,39 @@ class Traversal {
           q.unshift(newPath);
         }
       });
+
+      this.nextStage();
+      if (!this.currentStage) {
+        break;
+      }
+
+      this.currentStage.depth++;
     }
 
+    console.log(require('util').inspect(matchedPaths, { depth: null }));
     return matchedPaths;
   }
 
   isDestination (node) {
-    return Object.entries(this.filters).every(([key, val]) => {
+    return Object.entries(this.currentStage.filters).every(([key, val]) => {
       return node.attributes[key] === val;
     });
+  }
+
+  nextStage () {
+    if (this.currentStage.depth >= this.currentStage.maxDepth) {
+      this.stageIndex++;
+    }
+  }
+
+  get currentStage () {
+    return this.stages[this.stageIndex];
   }
 }
 
 class Query {
   constructor (graph, options) {
     this.graph = graph;
-    this.queryCriteria = [];
 
     this.traversal = new Traversal(graph);
   }
@@ -81,11 +140,56 @@ class Query {
   match (conditions) {
     this.traversal.filter(conditions);
 
+    this.traversal.commitStage();
+
     return this;
   }
 
+  nodes () {
+    // collect all the nodes in the paths
+    const nodes = {};
+    this.matchedPaths.forEach((path) => {
+      path.forEach((n) => {
+        nodes[n.id] = n;
+      });
+    });
+
+    return Object.values(nodes).filter((node) => {
+      let stageIndex = 0;
+      let stage = this.traversal.stages[stageIndex];
+
+      while (stage) {
+        if (Object.keys(stage.filters).length > 0) {
+          const pass = Object.entries(stage.filters).every(([key, val]) => {
+            return node.attributes[key] === val;
+          });
+
+          if (pass) {
+            return node;
+          }
+        }
+
+        stage = this.traversal.stages[stageIndex++];
+      }
+    });
+  }
+
+  any (options) {
+    if (options.maxDepth) {
+      this.traversal.currentStage.maxDepth = options.maxDepth;
+    }
+
+    return this;
+  }
+
+  paths () {
+    return this.matchedPaths;
+  }
+
   execute () {
-    return this.traversal.run();
+    this.matchedPaths = this.traversal.run();
+
+    return this;
   }
 }
 
--- compiler/graph/query.spec.js
+++ compiler/graph/query.spec.js
@@ -3,8 +3,8 @@
 const Graph = require('./graph');
 
 describe('Graph Query', () => {
-  describe('finds nodes by attributes', () => {
-    it('finds nodes that match criteria', () => {
+  describe('finds paths by attributes', () => {
+    it('finds paths that match criteria', () => {
       const graph = new Graph();
 
       const node1 = graph.addNode({ type: 'person' });
@@ -15,12 +15,58 @@ describe('Graph Query', () => {
 
       const q = graph.query();
 
-      q.match({ type: 'person' });
+      q.match({ type: 'person' }).execute();
 
-      expect(q.execute()).toEqual([
+      expect(q.paths()).toEqual([
         [node1, node2],
         [node1]
       ]);
+    });
+  });
+
+  describe('finds matchings nodes by attributes in a path', () => {
+    it('finds paths that match criteria', () => {
+      const graph = new Graph();
+
+      const node1 = graph.addNode({ type: 'person' });
+      const node2 = graph.addNode({ type: 'person' });
+      graph.addNode({ type: 'animal' });
+
+      graph.addEdge(node1, node2);
+
+      const q = graph.query();
+
+      q.match({ type: 'person' }).execute();
+
+      expect(q.nodes()).toEqual([node1, node2]);
+    });
+
+    it('returns all matching nodes', () => {
+      const graph = new Graph();
+
+      const node1 = graph.addNode({ name: 'node 1' });
+      const node2 = graph.addNode({ name: 'node 2' });
+      const node3 = graph.addNode({ name: 'node 3' });
+      const node4 = graph.addNode({ name: 'node 4' });
+      const node5 = graph.addNode({ name: 'node 5' });
+
+      graph.addEdge(node1, node2);
+      graph.addEdge(node2, node3);
+      graph.addEdge(node3, node4);
+      graph.addEdge(node4, node5);
+
+      const q = graph.query();
+
+      q.begin(node1)
+        .outgoing()
+        .any({ maxDepth: 2 })
+        .match({ name: 'node 3' })
+        .outgoing()
+        .any({ maxDepth: 2 })
+        .match({ name: 'node 5' })
+        .execute();
+
+      expect(q.nodes()).toEqual([node3, node5]);
     });
   });
 
@@ -41,9 +87,11 @@ describe('Graph Query', () => {
 
       q.begin(node1)
         .outgoing()
-        .match({ name: 'node 4' });
+        .any({ maxDepth: 3 })
+        .match({ name: 'node 4' })
+        .execute();
 
-      expect(q.execute()).toEqual([
+      expect(q.paths()).toEqual([
         [node1, node2, node3, node4]
       ]);
     });
@@ -56,6 +104,10 @@ describe('Graph Query', () => {
       const node3 = graph.addNode({ name: 'node 3' });
       const node4 = graph.addNode({ name: 'node 4' });
 
+      //   / 2 \
+      // 1      4
+      //   \ 3 /
+
       graph.addEdge(node1, node2);
       graph.addEdge(node1, node3);
       graph.addEdge(node2, node4);
@@ -65,9 +117,11 @@ describe('Graph Query', () => {
 
       q.begin(node1)
         .outgoing()
-        .match({ name: 'node 4' });
+        .any({ maxDepth: 3 })
+        .match({ name: 'node 4' })
+        .execute();
 
-      expect(q.execute()).toEqual([
+      expect(q.paths()).toEqual([
         [node1, node2, node4],
         [node1, node3, node4]
       ]);
@@ -91,16 +145,17 @@ describe('Graph Query', () => {
 
       q.begin(node1)
         .outgoing()
-        .match({ name: 'end node' });
+        .any({ maxDepth: 3 })
+        .match({ name: 'end node' })
+        .execute();
 
-      const res = q.execute();
-      expect(res[0]).toEqual([node1, node2, node3, node4]);
-      expect(res[1]).toEqual([node1, node2, node3, node5]);
+      expect(q.paths()[0]).toEqual([node1, node2, node3, node4]);
+      expect(q.paths()[1]).toEqual([node1, node2, node3, node5]);
     });
   });
 
   describe('multi-stage queries', () => {
-    it('finds a path with multiple traversal conditions', () => {
+    it.only('finds a path with multiple traversal conditions', () => {
       const graph = new Graph();
 
       const node1 = graph.addNode({ name: 'node 1' });
@@ -118,11 +173,14 @@ describe('Graph Query', () => {
 
       q.begin(node1)
         .outgoing()
+        .any({ maxDepth: 2 })
         .match({ name: 'node 3' })
         .outgoing()
-        .match({ name: 'node 5' });
+        .any({ maxDepth: 2 })
+        .match({ name: 'node 5' })
+        .execute();
 
-      expect(q.execute()).toEqual([
+      expect(q.paths()).toEqual([
         [node1, node2, node3, node4, node5]
       ]);
     });
