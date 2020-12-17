344628d0637d504c21c3e6a2022b7001603092d6
Dan Fairaizl <fairaizl@gmail.com>
2020-12-17 14:56:59 -0500

    Don't ignore build folder because codegen cannot emit files without it

--- runner/index.js
+++ runner/index.js
@@ -3,11 +3,11 @@ const { resolve } = require("path");
 
 const Compiler = require("../compiler");
 
-const sourceFile = resolve(__dirname, "..", "examples", "max.nv");
+const sourceFile = resolve(__dirname, "..", "examples", "binop.nv");
 
 const novaCompiler = new Compiler(
   sourceFile,
-  "max" /*, { debugGraph: true } */
+  "binop" /*, { debugGraph: true } */
 );
 
 novaCompiler.compile();
